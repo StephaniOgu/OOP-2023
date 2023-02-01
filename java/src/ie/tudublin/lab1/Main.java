ee0a0577d53074aee608198d345f532b1478745d
Stephanie <stefniogu@gmail.com>
2023-02-01 12:33:41 +0000

    GP lab2

--- java/src/ie/tudublin/lab1/Main.java
+++ java/src/ie/tudublin/lab1/Main.java
@@ -20,7 +20,7 @@ public class Main
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new BugZap());
     }
-	
+
 	public static void main(String[] args)
 	{
 		System.out.println("-- Dog executing -- \n");
@@ -28,6 +28,11 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
+		System.out.println("-- Cat executing -- \n");
+		Cat ginger = new Cat("Ginger");
+		for(int i = 0; i<9; i++){
+			ginger.kill();
+		}
 		loops();
 	}
 	
